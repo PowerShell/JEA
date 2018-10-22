8934c79f130778fe8c83c8a8d6eb50e13f9f5e3d
Chris Gardner <chris.gardner@section8games.co.uk>
2018-10-22 13:01:39 +0100

    Add extra tests for other possible cases

--- Tests/Unit/JeaRoleCapabilities.Tests.ps1
+++ Tests/Unit/JeaRoleCapabilities.Tests.ps1
@@ -17,6 +17,7 @@ Describe "Testing JeaRoleCapabilities" {
             It "Should return 2 hashtables with one having a nested hashtable" {
                 $Output = Convert-StringToObject -InputString "@{Name = 'Invoke-Cmdlet'; Parameters = @{Name = 'Parameter1';Value = 'Value1'},@{Name = 'Parameter2'; Value = 'Value2'}},@{Name = 'Invoke-Cmdlet2'}"
 
+                $Output.Count | Should -Be 2
                 $Output[0] | Should -BeOfType [Hashtable]
                 $Output[0].Name | Should -Be 'Invoke-Cmdlet'
                 ,$Output[0].Parameters | Should -BeOfType [Array]
@@ -33,10 +34,24 @@ Describe "Testing JeaRoleCapabilities" {
                 $Output | Should -Be "Invoke-Cmdlet"
             }
 
+            It "Should return a single hashtable when passed only one hashtable" {
+                $Output = Convert-StringToObject -InputString "@{Name = 'Invoke-Cmdlet'}"
+
+                $Output | Should -BeOfType [Hashtable]
+                $Output.Name | Should -Be 'Invoke-Cmdlet'
+            }
+
             It "Should return 2 strings when passed 2 comma separated strings in a single string" {
                 $Output = Convert-StringToObject -InputString "'Invoke-Cmdlet','Invoke-Cmdlet2'"
 
                 $Output | Should -Be 'Invoke-Cmdlet','Invoke-Cmdlet2'
+            }
+
+            It "Should not call New-Item when parsing the string input that contains an escaped subexpression" {
+                Mock -CommandName New-Item -MockWith {}
+                $null = Convert-StringToObject -InputString "`$(New-Item File.txt),'Invoke-Cmdlet'"
+
+                Assert-MockCalled -CommandName New-Item -Times 0 -Scope It
             }
         }
     }
