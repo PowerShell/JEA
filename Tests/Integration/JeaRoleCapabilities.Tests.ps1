86fbb369083bfb0de4617dc420b194e32318aec1
Chris Gardner <chris.gardner@section8games.co.uk>
2018-12-26 19:22:28 +0000

    Add some integration tests for JeaRoleCapabilties to test individula methods

--- Tests/Integration/JeaRoleCapabilities.Tests.ps1
+++ Tests/Integration/JeaRoleCapabilities.Tests.ps1
@@ -11,20 +11,74 @@ Describe "Integration testing JeaRoleCapabilities" -Tag Integration {
         $class.Path = 'TestDrive:\ModuleFolder\RoleCapabilities\ExampleRole.psrc'
     }
 
-    Context "Testing Set method" {
 
-        It "Should remove the role capabilities file when Ensure is set to Absent" {
-            New-Item -Path 'TestDrive:\RemoveMe\RoleCapabilities\ExampleRole.psrc' -Force
+    Context "Testing Get method when Ensure is Present" {
 
-            $class.Ensure = [Ensure]::Absent
-            $class.Path = 'TestDrive:\RemoveMe\RoleCapabilities\ExampleRole.psrc'
-            $class.Set()
+        It "Should return an object of JeaRoleCapabilities type" {
 
-            Test-Path -Path 'TestDrive:\RemoveMe\RoleCapabilities\ExampleRole.psrc' | Should -Be $false
+            $null = New-Item -Path $class.Path -Force
+            $null = New-PSRoleCapabilityFile -Path $class.Path -VisibleCmdlets 'Get-Command'
 
+            $Result = $class.Get()
+
+            $Result.GetType().Name | Should -Be 'JeaRoleCapabilities'
         }
 
-        It "Should create a new RoleCapbailities folder and populate with a psrc file with 1 visible function when Ensure is set to Present" {
+        It "Should remove Copyright, GUID, Author and CompanyName from the object after importing the psrc" {
+
+            $null = New-Item -Path $class.Path -Force
+            $null = New-PSRoleCapabilityFile -Path $class.Path -VisibleCmdlets 'Get-Command'
+
+            $Result = $class.Get()
+
+            $Result.PSObject.Properties | Should -Not -Contain 'Copyright'
+            $Result.PSObject.Properties | Should -Not -Contain 'GUID'
+            $Result.PSObject.Properties | Should -Not -Contain 'Author'
+            $Result.PSObject.Properties | Should -Not -Contain 'CompanyName'
+        }
+
+        It "Should return an object with property Ensure set to Present" {
+
+            $null = New-Item -Path $class.Path -Force
+            $null = New-PSRoleCapabilityFile -Path $class.Path -VisibleCmdlets 'Get-Command'
+
+            $Result = $class.Get()
+
+            $Result.Ensure | Should -Be 'Present'
+        }
+
+        It "Should populate returned properties from VisibleCmdlets in psrc file" {
+
+            $null = New-Item -Path $class.Path -Force
+            $null = New-PSRoleCapabilityFile -Path $class.Path -VisibleCmdlets 'Get-Command'
+
+            $Result = $class.Get()
+
+            $Result.VisibleCmdlets | Should -Be 'Get-Command'
+        }
+
+    }
+
+    Context "Testing Get method when Ensure is Absent" {
+
+        It "Should return an object of JeaRoleCapabilities type" {
+
+            $Result = $class.Get()
+
+            $Result.GetType().Name | Should -Be 'JeaRoleCapabilities'
+        }
+
+        It "Should return an object with property Ensure set to Present" {
+
+            $Result = $class.Get()
+
+            $Result.Ensure | Should -Be 'Absent'
+        }
+    }
+
+    Context "Testing Set method when Ensure is Present" {
+
+        It "Should create a new RoleCapbailities folder and populate with a psrc file with 1 visible function" {
             $class.VisibleFunctions = 'Get-Service'
             $class.Set()
 
@@ -33,7 +87,7 @@ Describe "Integration testing JeaRoleCapabilities" -Tag Integration {
             $result.VisibleFunctions | Should -Be 'Get-Service'
         }
 
-        It "Should create a new psrc with 1 visible function and all Get cmdlets visible when Ensure is set to Present" {
+        It "Should create a new psrc with 1 visible function and all Get cmdlets visible" {
             $class.VisibleCmdlets = 'Get-*'
             $class.VisibleFunctions = 'New-Example'
             $class.Set()
@@ -44,7 +98,7 @@ Describe "Integration testing JeaRoleCapabilities" -Tag Integration {
             $result.VisibleCmdlets | Should -Be 'Get-*'
         }
 
-        It "Should create a new psrc with 1 visible function and all cmdlets in DnsServer module and Restart-Service visible when Ensure is set to Present" {
+        It "Should create a new psrc from an array of strings with 1 visible function and all cmdlets in DnsServer module and Restart-Service visible" {
             $class.VisibleCmdlets = 'DnsServer\*', "@{'Name' = 'Restart-Service';'Parameters' = @{'Name' = 'Name';'ValidateSet' = 'Dns' } }"
             $class.VisibleFunctions = 'New-Example'
             $class.Set()
@@ -57,6 +111,64 @@ Describe "Integration testing JeaRoleCapabilities" -Tag Integration {
             $result.VisibleCmdlets[1].Name | Should -Be 'Restart-Service'
             $result.VisibleCmdlets[1].Parameters.Name | Should -Be 'Name'
             $result.VisibleCmdlets[1].Parameters.ValidateSet | Should -Be 'Dns'
+        }
+
+        It "Should create a new psrc from a single string with 1 visible function and all cmdlets in DnsServer module and Restart-Service visible" {
+            $class.VisibleCmdlets = 'DnsServer\*, @{"Name" = "Restart-Service";"Parameters" = @{"Name" = "Name";"ValidateSet" = "Dns" } }'
+            $class.VisibleFunctions = 'New-Example'
+            $class.Set()
+
+            Test-Path -Path $class.Path | Should -Be $true
+            $result = Import-PowerShellDataFile -Path $class.Path
+            $result.VisibleFunctions | Should -Be 'New-Example'
+            $result.VisibleCmdlets[0] | Should -Be 'DnsServer\*'
+            $result.VisibleCmdlets[1] | Should -BeOfType [Hashtable]
+            $result.VisibleCmdlets[1].Name | Should -Be 'Restart-Service'
+            $result.VisibleCmdlets[1].Parameters.Name | Should -Be 'Name'
+            $result.VisibleCmdlets[1].Parameters.ValidateSet | Should -Be 'Dns'
+        }
+
+        It "Should create a psrc with a function definition and a visible function for that custom function" {
+            $class.FunctionDefinitions = "@{Name = 'Get-ExampleFunction'; ScriptBlock = {Get-Command} }"
+            $class.VisibleFunctions = 'Get-ExampleFunction'
+            $class.Set()
+
+            Test-Path -Path $class.Path | Should -Be $true
+            $result = Import-PowerShellDataFile -Path $class.Path
+            $result.VisibleFunctions | Should -Be 'Get-ExampleFunction'
+            $result.FunctionDefinitions.Name | Should -Be 'Get-ExampleFunction'
+            $result.FunctionDefinitions.Scriptblock | Should -Be '{Get-Command}'
+            $result.FunctionDefinitions.Scriptblock | Should -BeOfType [ScriptBlock]
+        }
+
+        It "Should create a psrc with 2 function definitions and 2 visible function for those custom function" {
+            $class.FunctionDefinitions = "@{Name = 'Get-ExampleFunction'; ScriptBlock = {Get-Command} }","@{Name = 'Get-OtherExample'; ScriptBlock = {Get-Command} }"
+            $class.VisibleFunctions = 'Get-ExampleFunction','Get-OtherExample'
+            $class.Set()
+
+            Test-Path -Path $class.Path | Should -Be $true
+            $result = Import-PowerShellDataFile -Path $class.Path
+            $result.VisibleFunctions | Should -Be 'Get-ExampleFunction','Get-OtherExample'
+            $result.FunctionDefinitions[0].Name | Should -Be 'Get-ExampleFunction'
+            $result.FunctionDefinitions[0].Scriptblock | Should -Be '{Get-Command}'
+            $result.FunctionDefinitions[0].Scriptblock | Should -BeOfType [ScriptBlock]
+            $result.FunctionDefinitions[1].Name | Should -Be 'Get-OtherExample'
+            $result.FunctionDefinitions[1].Scriptblock | Should -Be '{Get-Command}'
+            $result.FunctionDefinitions[1].Scriptblock | Should -BeOfType [ScriptBlock]
+        }
+    }
+
+    Context "Testing Set method when Ensure is Absent" {
+
+        It "Should remove the role capabilities file" {
+            New-Item -Path 'TestDrive:\RemoveMe\RoleCapabilities\ExampleRole.psrc' -Force
+
+            $class.Ensure = [Ensure]::Absent
+            $class.Path = 'TestDrive:\RemoveMe\RoleCapabilities\ExampleRole.psrc'
+            $class.Set()
+
+            Test-Path -Path 'TestDrive:\RemoveMe\RoleCapabilities\ExampleRole.psrc' | Should -Be $false
+
         }
     }
 
